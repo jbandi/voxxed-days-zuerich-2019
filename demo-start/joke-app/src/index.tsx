7d03910f2e71f0f8750c02d65e0abc9b7cb43381
Jonas Bandi <dev@jonasbandi.net>
2019-03-16 01:16:18 +0100

    Removing platform activator

--- demo-start/joke-app/src/index.tsx
+++ demo-start/joke-app/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { PlatformActivator } from '@scion/workbench-application.core';
-
-PlatformActivator.start();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
